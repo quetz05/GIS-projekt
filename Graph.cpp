88b16977c68f45bfda19e13d631e08364a32333a
quetz05 <quetz05@gmail.com>
2014-10-28 19:41:16 +0100

    Stworzenie klasy grafu cz. 2

---
+++ Graph.cpp
@@ -0,0 +1,100 @@
+#include "Graph.h"
+
+namespace GIS
+{
+
+
+	Node::Node(int id)
+	{
+
+
+	}
+
+	bool Node::hasConnection(int nodeId) const
+	{
+
+		return false;
+	}
+
+
+
+	Graph::Graph()
+	{
+
+
+	}
+
+	Graph::Graph(int nodeAmount)
+	{
+
+
+	}
+
+
+	Graph::Graph(std::string fileName)
+	{
+
+
+	}
+
+	Graph::~Graph()
+	{
+
+
+	}
+
+
+	const Node& Graph::getNode(int nodeId) const
+	{
+
+		return false;
+	}
+
+
+	int Graph::getSize() const
+	{
+
+		return false;
+
+	}
+
+
+	void Graph::addPath(int nodeId, int connectedNodeId, int weight)
+	{
+
+
+	}
+
+
+	void Graph::addConnection(int nodeId, const Path& path)
+	{
+
+
+	}
+
+	void Graph::addNode()
+	{
+
+
+	}
+
+	void Graph::addNode(const Node& newNode)
+	{
+
+
+	}
+
+
+	void Graph::addNode(int id)
+	{
+
+
+	}
+
+
+
+
+
+
+
+}
